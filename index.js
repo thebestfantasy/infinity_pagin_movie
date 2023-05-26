b3e95c08f6553315099ed43c61c59f19ce4bc8ae
thebestfantasy <sergej.fronch@gmail.com>
2023-05-26 05:14:47 +0300

    fdf

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
+// intersectionObserver
+
 const BASE_URL = 'https://api.themoviedb.org/3';
 const ENDPOINT = '/trending/movie/week'
 const API_KEY = '345007f9ab440e5b86cef51be6397df1'; 
 const list = document.querySelector('.js-list');
-const loadMore = document.querySelector('.js-load-more');
+const guard = document.querySelector('.js-guard');
 let page = 1;
 
-loadMore.addEventListener('click', pagination);
-
-function pagination() {
-    page += 1;
-    serviceMovie(page)
-        .then(data => {
-            list.insertAdjacentHTML('beforeend', createMarkup(data.results))  ///results это с бекэнда
-            if (data.total_pages <= data.page) {
-                loadMore.hidden = true;
-            }
-        })
-        .catch(err => console.log(err));
+let options = {
+    root: null,  //null - весь viewportwidth, если null можно не указывать
+    rootMargin: '300px', //срабатывание за 300px до конца
+    treshold: 0, //если 0 можно не указывать, переход границы вьюпорта 1=100%
 }
-// const options = {
-//     method: 'GET',
-//     headers: {
-//         Authorization: `Bearer ${TOKEN}`
-//     }
+
+let observer = new IntersectionObserver(handlerPagination, options);
+
+function handlerPagination(entries) {
+    entries.forEach(entry => {
+        
+    });
+};
+
+// function pagination() {
+//     page += 1;
+//     serviceMovie(page)
+//         .then(data => {
+//             list.insertAdjacentHTML('beforeend', createMarkup(data.results))  ///results это с бекэнда
+//             if (data.total_pages <= data.page) {
+//                 loadMore.hidden = true;
+//             }
+//         })
+//         .catch(err => console.log(err));
 // }
 
 function serviceMovie(page = 1) {
@@ -39,7 +47,7 @@ serviceMovie()
     .then(data => {
         list.insertAdjacentHTML('beforeend', createMarkup(data.results))  ///results это с бекэнда
         if (data.total_pages > data.page) {
-            loadMore.hidden = false;          /////кнопка будет показана до последней страницы, на последней аттрибут hidden из html возвращается
+            observer.observe(guard)
         }
     })
     .catch(err => console.log(err));
